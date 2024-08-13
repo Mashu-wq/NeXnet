0cee6ed684c745b5db96602ea991ef612e1452a0
Mayesha <mim2007088@stud.kuet.ac.bd>
2024-08-13 14:46:00 +0600

    added new

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color appColor1 = Color.fromARGB(255, 204, 218, 225);
+const Color whiteColor = Colors.white;
+const Color blackColor = Colors.black;
+const Color blackColor45 = Colors.black45;
+const Color buttonColor = Colors.white70;
+const Color appColor2 = Color.fromARGB(255, 83, 136, 162);
