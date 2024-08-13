e447f11a7805d0a6eae7040c7356280858d6ac72
Mayesha <mim2007088@stud.kuet.ac.bd>
2024-08-13 13:45:25 +0600

    updated generics exceptions

--- lib/services/auth/auth_exceptions.dart
+++ lib/services/auth/auth_exceptions.dart
@@ -1,12 +1,16 @@
 // login exceptions
 class UserNotFoundException implements Exception {}
+
 class WrongPasswordException implements Exception {}
 
 // register exceptions
 class WeekPasswordException implements Exception {}
+
 class EmailAlreadyInUseException implements Exception {}
+
 class InvalidEmailException implements Exception {}
 
-// generic exceptions
+// generic exceptions(Authentication related errors that don't fall under the specific categories)
 class GenericAuthException implements Exception {}
+
 class UserNotLoggedInException implements Exception {}
